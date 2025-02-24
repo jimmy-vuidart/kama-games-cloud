cf02bb4a0ba7e05d872a9e55c080e9acdf67561c
Kamalen <tidiusff@gmail.com>
2025-02-24 09:59:52 +0100

    Prepare frontend for nginx deployment

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, config);
   app.enableCors();
-  app.setGlobalPrefix('kilsiworld/api');
+  // app.setGlobalPrefix('kilsiworld/api');
   await app.listen(process.env.PORT || 3000);
 }
 
