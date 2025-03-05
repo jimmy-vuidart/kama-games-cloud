d294087b12bb6d617f94426d3dcfb7f69c442aec
Kamalen <tidiusff@gmail.com>
2025-03-05 10:55:18 +0100

    Finally a complete docker environment working

--- apps/front/src/app/app.config.ts
+++ apps/front/src/app/app.config.ts
@@ -3,10 +3,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes'
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimationsAsync()],
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideClientHydration(withEventReplay())],
 }
--- apps/front/src/environments/environment.prod.ts
+++ apps/front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 
 export const environment = {
-  apiUrl: 'https://kamalen.duckdns.org/kama-games-cloud/api',
+  apiUrl: '/kama-games/api',
 }
